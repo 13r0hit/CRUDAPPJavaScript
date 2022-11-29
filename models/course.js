2854859141b8aeed09af3d93ccf05f3b23700043
13r0hit <87716502+13r0hit@users.noreply.github.com>
2022-11-29 16:18:07 -0500

    models are added

---
+++ models/course.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const coursesSchemaDefinition = {
+    name: {
+        type: String,
+        required: true
+    },
+    Status:{
+        type:String,
+        required:true
+    }
+};
+
+var coursesSchema = new mongoose.Schema(coursesSchemaDefinition);
+module.exports = mongoose.model('Course', coursesSchema);
